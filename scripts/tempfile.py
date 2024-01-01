58cb945448c1d986c1fefe680ec081a6b5bac810
highghlow <highghlow@proton.me>
2024-01-01 22:52:03 +0300

    Added tempfile script

---
+++ scripts/tempfile.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import uuid
+
+def main():
+    args = sys.argv[1:]
+
+    filename = "/tmp/" + uuid.uuid4().hex
+
+    with open(filename, "w") as f:
+        f.write(" ".join(args))
+
+    print(filename)
+
+if __name__ == "__main__":
+    main()
